383f9f42d3dc3c02008a6c7fc508cace19ed7eae
robert16 <robertmur60@gmail.com>
2019-11-22 12:46:44 -0500

    nueva adicion de carpetas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 
-const routes: Routes = [];
+
+const APP_ROUTES: Routes = [
+{path:'about', component: AboutComponent},
+{path:'heroes',component: HeroesComponent},
+{path:'home',  component: HomeComponent},
+{path:'navbar',component: NavbarComponent},
+{path:'heroe/:id', component: HeroeComponent},
+
+{path:'**', pathMatch:'full', redirectTo:'home'}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//servicios
+import { HeroesService } from './services/heroes.service';
+
+//rutas
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeroesComponent,
+    HomeComponent,
+    NavbarComponent,
+    HeroeComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
